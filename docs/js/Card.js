3f0efebe3fb7ae19801325a1bf99623d90074759
Richard Lara <richardlara67@gmail.com>
2019-10-23 16:07:28 -0500

    Update Card.js

--- docs/js/Card.js
+++ docs/js/Card.js
@@ -1,9 +1,10 @@
 /**
- *
- * Author: Maximo Mena
- * GitHub: mmenavas
- * Twitter: @menamaximo
- * Project: Memory Workout
+ * Author: DeVry University CEIS420 Team 3
+ * Team leader: Richard Lara
+ * Team members: Richard Lara, Juan Diaz, Manuel Gonzalez, 
+ * Joey Gil, Devin Nguyen, Logan Susini, Mohammad Mohammad
+ * GitHub: https://github.com/richardlara67/PairPort
+ * Project: PairPort
  * Description: A JS, HTML and CSS based memory game.
  * The goal is to match pairs of cards in the least
  * number of matching attempts.
